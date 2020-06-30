303720bf16f5ac2c4c015dfe15899e6253941a74
Leo James <66539145+leohjames@users.noreply.github.com>
2020-06-30 14:41:25 +0100

    Update PrimeDirective.java

--- PrimeDirective.java
+++ PrimeDirective.java
@@ -24,7 +24,6 @@ class PrimeDirective {
         return primes;
     }
     public static void main(String[] args) {
-
         PrimeDirective pd = new PrimeDirective();
         int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
         System.out.println(pd.onlyPrimes(numbers));
