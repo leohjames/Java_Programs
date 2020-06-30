cf02599817b66f92bb65fd62f8921b18ba4cc345
Leo James <66539145+leohjames@users.noreply.github.com>
2020-06-30 14:41:07 +0100

    Update PrimeDirective.java

--- PrimeDirective.java
+++ PrimeDirective.java
@@ -1,14 +1,12 @@
+// This program creates a list of prime numbers given a list of numbers and sorting though them to see if the numbers are prime or not
 import java.util.ArrayList;
 class PrimeDirective {
-
     public boolean isPrime(int number) {
-
         if (number == 2) {
             return true;
         } else if (number < 2) {
             return false;
         }
-
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 return false;
@@ -16,7 +14,6 @@ class PrimeDirective {
         }
         return true;
     }
-
     public ArrayList<Integer> onlyPrimes(int[] numbers) {
         ArrayList<Integer> primes = new ArrayList<Integer>();
         for (int number : numbers) {
@@ -26,13 +23,10 @@ class PrimeDirective {
         }
         return primes;
     }
-
-
     public static void main(String[] args) {
 
         PrimeDirective pd = new PrimeDirective();
         int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
         System.out.println(pd.onlyPrimes(numbers));
     }
-
 }
